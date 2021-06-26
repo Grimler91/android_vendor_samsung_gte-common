fcf6c570acefaf0d6e96b44746e3c165e2d1b76d
Henrik Grimler <henrik@grimler.se>
2021-06-26 13:25:10 +0200

    gte-common: add qseecom files

--- gte-common-vendor.mk
+++ gte-common-vendor.mk
@@ -6,7 +6,9 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/samsung/gte-common
 
 PRODUCT_COPY_FILES += \
+    vendor/samsung/gte-common/proprietary/bin/qseecomd:$(TARGET_COPY_OUT_SYSTEM)/bin/qseecomd \
     vendor/samsung/gte-common/proprietary/lib/libaudiosa.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libaudiosa.so \
     vendor/samsung/gte-common/proprietary/lib/libmysound.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libmysound.so \
     vendor/samsung/gte-common/proprietary/lib/soundfx/libaudioeffectoffload.so:$(TARGET_COPY_OUT_SYSTEM)/lib/soundfx/libaudioeffectoffload.so \
-    vendor/samsung/gte-common/proprietary/lib/soundfx/libmyspace.so:$(TARGET_COPY_OUT_SYSTEM)/lib/soundfx/libmyspace.so
+    vendor/samsung/gte-common/proprietary/lib/soundfx/libmyspace.so:$(TARGET_COPY_OUT_SYSTEM)/lib/soundfx/libmyspace.so \
+    vendor/samsung/gte-common/proprietary/vendor/lib/libQSEEComAPI.so:$(TARGET_COPY_OUT_VENDOR)/lib/libQSEEComAPI.so
